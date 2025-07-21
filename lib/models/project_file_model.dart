b0ecddff85eb776f55a48428198d74d9dbb1dd6f
Charlie9830 <charliewjhall@gmail.com>
2025-07-21 18:31:03 +1000

    Fix: Proper serialization of fileVersion property.

--- lib/models/project_file_model.dart
+++ lib/models/project_file_model.dart
@@ -3,22 +3,32 @@ import 'dart:convert';
 
 import 'package:pingadinga/models/device_model.dart';
 
+const _kFileVersion = 1;
+
 class ProjectFileModel {
-  final int fileVersion = 1;
+  final int fileVersion;
   final List<DeviceModel> devices;
 
-  ProjectFileModel({required this.devices});
+  ProjectFileModel({required this.devices, this.fileVersion = _kFileVersion});
 
   ProjectFileModel copyWith({List<DeviceModel>? devices}) {
     return ProjectFileModel(devices: devices ?? this.devices);
   }
 
   Map<String, dynamic> toMap() {
-    return <String, dynamic>{'devices': devices.map((x) => x.toMap()).toList()};
+    return <String, dynamic>{
+      'fileVersion': fileVersion,
+      'devices': devices.map((x) => x.toMap()).toList(),
+    };
   }
 
   factory ProjectFileModel.fromMap(Map<String, dynamic> map) {
+    final assertedFileVersion =
+        map['fileVersion'] ??
+        1; // Assert the fileVersion property to 1 if it doesn't exist.
+
     return ProjectFileModel(
+      fileVersion: assertedFileVersion,
       devices: List<DeviceModel>.from(
         (map['devices']).map<DeviceModel>(
           (x) => DeviceModel.fromMap(x as Map<String, dynamic>),
